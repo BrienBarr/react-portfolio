653488b3cbc9a6d6671f2a4e01a5e9eb3e6fcd03
BrienB <barrdom@hotmail.com>
2021-04-18 16:58:08 -0400

    updated footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,20 @@
 import React from "react";
+import "./style.css";
+
+const styles = {
+  bold: {fontWeight: 'bold'},
+}
 
 function Footer () {
   return (
-    <div>Footer</div>
+    <footer className="footer footer-dark bg-dark">
+      <div className="container">
+        <span style={styles.bold}>Brien Barr</span>
+        <br/>
+        <a href="mailto:brien.barr3320@gmail.com">brien.barr3320@gmail.com</a><br/>
+        PH: 317-910-6049
+      </div>
+    </footer>
   )
 }
 
