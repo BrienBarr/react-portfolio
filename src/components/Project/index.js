b47c1edcdb852faeba9ad274684aeee34e39081e
BrienB <barrdom@hotmail.com>
2021-04-19 01:19:13 -0400

    updated Project component to pass in project info from projects.json file

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,8 +1,37 @@
 import React from "react";
 
-function Project () {
+function Project (props) {
   return (
-    <div>Project</div>
+    <div className="col-lg-4">          
+      <div className="card smallcard">
+        <img 
+          className="card-img-top img-thumbnail medium" 
+          src={props.image} 
+          title={props.projectName} 
+          alt={props.projectName}
+        />
+        <div className="card-body">
+          <p className="card-text">
+            <a 
+              href={props.deployed} 
+              target="_blank" 
+              rel="noreferrer"
+            >
+              {props.projectName}
+            </a>
+            <br/>
+            {props.repoHost}: 
+            <a 
+              href={props.repository} 
+              target="_blank" 
+              rel="noreferrer"
+            >
+              {props.repoName}
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,15 +1,40 @@
 import React, { Component } from "react";
 import Project from '../components/Project';
+import projects from "../projects.json";
 
 class Portfolio extends Component {
 
-  render() {
+  state = {
+    projects
+  };
+
+
+  render(){
     return (
-      <div>
-        <Project />
+      <div className="portfolio">
+
+        <div className="row">
+          <div className="col-lg-12">
+            <h1>Portfolio</h1>
+          </div>
+        </div>
+
+        <div className="row center">
+          {this.state.projects.map(item => (
+            <Project
+              projectName={item.projectName}
+              image={item.image}
+              deployed={item.deployed}
+              repoHost={item.repoHost}
+              repoName={item.repoName}
+              repository={item.repository}
+            />
+          ))}
+        </div>
+
       </div>
-    );
+    )
   }
 }
 
-export default Portfolio;
+export default Portfolio
